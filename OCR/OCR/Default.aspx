d59be5be6c743214f8263fdd6da9a37c06b0a6b4
shivam01990 <shivam01990@gmail.com>
2016-06-11 12:03:40 +0530

    Tessract OCR

---
+++ OCR/OCR/Default.aspx
@@ -0,0 +1,156 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" EnableEventValidation="true" Inherits="OCRExtractTable.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>OCR App</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <style>
+        body {
+            padding-top: 70px;
+            /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+        }
+    </style>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+    <link href="content/bootstrap-fileinput/css/fileinput.css" rel="stylesheet" />
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script src="js/jquery.browser.js"></script>
+    <script type="text/javascript" src="js/fileinput.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script src="js/fileinput_locale_LANG.js"></script>
+    <script src="js/jquery.Jcrop.js"></script>
+    <link href="css/jquery.Jcrop.css" type="text/css" rel="stylesheet" />
+
+    <script type="text/javascript">
+        var rootpath = '<%=Page.ResolveUrl("~")%>';
+        $(document).ready(function () {
+            $("#file_BrandImage").fileinput({
+                uploadUrl: rootpath + 'FileUploadHandler.ashx',
+                uploadAsync: true,
+                allowedFileExtensions: ['jpg', 'png', 'gif']
+            });
+
+            $("#file_BrandImage").on('fileuploaded', function (event, data, previewId, index) {
+                //var form = data.form, files = data.files, extra = data.extra,
+                //    response = data.response, reader = data.reader; 
+                $.each(data.files, function (k, obj) {
+                    $("[id$=hdnUploadedImage]").val(obj["name"]);
+                    $("[id$=imgUpload]").attr('src', rootpath + "uploads/" + $("[id$=hdnUploadedImage]").val())
+                    $("[id$=ocr-sec]").show();
+
+                    $('#<%=imgUpload.ClientID%>').Jcrop({
+                        onSelect: SelectCropArea
+                    });
+
+                });
+            })
+
+            $('#file_BrandImage').on('fileclear', function (event) {
+                $("[id$=hdnUploadedImage]").val('');
+                $("[id$=ocr-sec]").hide();
+            });
+
+        });
+
+
+        function SelectCropArea(c) {
+            $('#<%=X.ClientID%>').val(parseInt(c.x));
+            $('#<%=Y.ClientID%>').val(parseInt(c.y));
+            $('#<%=W.ClientID%>').val(parseInt(c.w));
+            $('#<%=H.ClientID%>').val(parseInt(c.h));
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">OCR APP</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+
+        <!-- Page Content -->
+        <div class="container">
+            <div class="row">
+                <h2>
+                    <label>Upload File</label></h2>
+                <div class="col-sm-12 text-center">
+                    <input id="file_BrandImage" type="file" data-min-file-count="1">
+                </div>
+            </div>
+            <!-- /.row -->
+            <div id="ocr-sec" style="display: none;" class="col-sm-12">
+                <div class="row">
+                    <h2>
+                        <label>Upload File Preview</label></h2>
+                    <div class="form-horizontal">
+                        <div class="form-group">
+
+                            <asp:Image ID="imgUpload" runat="server" />
+                        </div>
+                        <div class="form-group">
+                            <label for="inputEmail3" class="col-sm-2 control-label">Total Columns</label>
+                            <div class="col-sm-10">
+                                <asp:TextBox ID="txtColumns" runat="server" class="form-control" placeholder="Columns"></asp:TextBox>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputEmail3" class="col-sm-2 control-label">Total Rows</label>
+                            <div class="col-sm-10">
+                                <asp:TextBox ID="txtRows" runat="server" class="form-control" placeholder="Columns"></asp:TextBox>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <asp:Button ID="btnOCRReader" runat="server" Text="Read Image Data" CssClass="btn btn-primary" OnClick="btnOCRReader_Click" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <asp:Label ID="lblText" runat="server" Text="Label"></asp:Label>
+        </div>
+        <!-- /.container -->
+        <asp:HiddenField ID="hdnUploadedImage" runat="server" />
+        <asp:HiddenField ID="X" runat="server" />
+        <asp:HiddenField ID="Y" runat="server" />
+        <asp:HiddenField ID="W" runat="server" />
+        <asp:HiddenField ID="H" runat="server" />
+    </form>
+</body>
+</html>
---
+++ OCR/OCR/Default.aspx.cs
@@ -0,0 +1,209 @@
+﻿
+using Patagames.Ocr;
+using Patagames.Ocr.Enums;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OCRExtractTable
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+            }
+        }
+
+        protected void btnOCRReader_Click(object sender, EventArgs e)
+        {
+            string filePath = Server.MapPath("~/uploads/" + Path.GetFileName(hdnUploadedImage.Value));
+
+            // Crop Image Here & Save
+            string fileName = Path.GetFileName(filePath);
+            string cropFileName = "";
+            string cropFilePath = "";
+            if (File.Exists(filePath))
+            {
+                System.Drawing.Image orgImg = System.Drawing.Image.FromFile(filePath);
+                Rectangle CropArea = new Rectangle(
+                    Convert.ToInt32(X.Value),
+                    Convert.ToInt32(Y.Value),
+                    Convert.ToInt32(W.Value),
+                    Convert.ToInt32(H.Value));
+                try
+                {
+                    Bitmap bitMap = new Bitmap(CropArea.Width, CropArea.Height);
+                    using (Graphics g = Graphics.FromImage(bitMap))
+                    {
+                        g.DrawImage(orgImg, new Rectangle(0, 0, bitMap.Width, bitMap.Height), CropArea, GraphicsUnit.Pixel);
+                    }
+                    cropFileName = "crop_" + fileName;
+                    cropFilePath = Path.Combine(Server.MapPath("~/uploads"), cropFileName);
+                    bitMap.Save(cropFilePath);
+                    //Response.Redirect("~/UploadImages/" + cropFileName, false);
+                }
+                catch (Exception ex)
+                {
+                    //throw;
+                }
+            }
+            string extractText = this.ExtractTextFromImage(cropFilePath);
+            lblText.Text = extractText.Replace(Environment.NewLine, "<br />");
+        }
+
+        private string ExtractTextFromImage(string filePath)
+        {
+            string plainText = "";
+            string datapath = Server.MapPath("~");
+            List<System.Drawing.Image> cropimages = new List<System.Drawing.Image>();
+            System.Drawing.Image img = System.Drawing.Image.FromFile(filePath, true);
+
+            int totalColumns = 0;
+            int.TryParse(txtColumns.Text, out totalColumns);
+            int totalRows = 0;
+            int.TryParse(txtRows.Text, out totalRows);
+            cropimages = MultiCrop(filePath, img, totalRows, totalColumns);
+
+            string directorypath = Server.MapPath("~/uploads/") + Path.GetFileNameWithoutExtension(filePath);
+            if (!Directory.Exists(directorypath)) ;
+            {
+                Directory.CreateDirectory(directorypath);
+            }
+
+            DataTable dt = new DataTable();
+            for (int i = 1; i <= totalColumns; i++)
+            {
+                dt.Columns.Add("Column" + i);
+            }
+            List<string> lstdata = new List<string>();
+
+            for (int i = 0; i < cropimages.Count; i++)
+            {
+                {
+                    string temp_crop_file = directorypath + "\\" + i + ".bmp";
+                    {
+                        cropimages[i].Save(temp_crop_file);
+
+                    }
+                    try
+                    {
+                        using (var api = OcrApi.Create())
+                        {
+                            api.Init(Languages.English, datapath);
+                            using (var bmp = Bitmap.FromFile(temp_crop_file) as Bitmap)
+                            {
+                                lstdata.Add(api.GetTextFromImage(bmp));
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        lstdata.Add("");
+                    }
+                }
+            }
+
+            for (int i = 0; i < lstdata.Count; i++)
+            {
+                DataRow dr = dt.NewRow();
+                for (int j = 0; j < totalColumns; j++)
+                {
+                    dr[j] = lstdata[i];
+                    i++;
+
+                    if (i >= lstdata.Count)
+                    {
+                        break;
+                    }
+                }
+                i--;
+                dt.Rows.Add(dr);
+            }
+
+            GenerateReport(dt);
+            string extractedText = "";
+            //string extractedText = modiImage.Layout.Text;
+            //modiDocument.Close();
+            return extractedText;
+        }
+
+        public List<System.Drawing.Image> MultiCrop(string filepath, System.Drawing.Image img, int row, int col)
+        {
+            List<System.Drawing.Image> list = new List<System.Drawing.Image>();
+            Graphics g = Graphics.FromImage(img);
+            Brush redBrush = new SolidBrush(Color.Red);
+            Pen pen = new Pen(redBrush, 3);
+            for (int i = 0; i < row; i++)
+            {
+                for (int y = 0; y < col; y++)
+                {
+                    System.Drawing.Image temp = System.Drawing.Image.FromFile(filepath, true);
+
+
+                    Rectangle r = new Rectangle(y * (img.Width / col),
+                                                i * (img.Height / row),
+                                                img.Width / col,
+                                                img.Height / row);
+
+                    g.DrawRectangle(pen, r);
+
+                    list.Add(cropImage(temp, r));
+                }
+            }
+            return list;
+        }
+
+        private System.Drawing.Image cropImage(System.Drawing.Image img, Rectangle cropArea)
+        {
+            Bitmap bmpImage = new Bitmap(img);
+            Bitmap bmpCrop = bmpImage.Clone(cropArea, System.Drawing.Imaging.PixelFormat.DontCare);
+            img.Dispose();
+            return (System.Drawing.Image)(bmpCrop);
+
+        }
+
+
+        #region--Generate Excel--
+        protected void GenerateReport(DataTable dt)
+        {
+            Response.ClearContent();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", string.Format("attachment; filename={0}", "ToDaysOfferAddedReport" + DateTime.Now.ToString("dd_MM_yyyy_hh_mm_ss") + ".xls"));
+            Response.ContentType = "application/ms-excel";
+
+            string tab = string.Empty;
+
+            foreach (DataColumn dc in dt.Columns)
+            {
+                Response.Write(tab + dc.ColumnName);
+                tab = "\t";
+            }
+            Response.Write("\n");
+            int i;
+            foreach (DataRow dr in dt.Rows)
+            {
+                tab = "";
+                for (i = 0; i < dt.Columns.Count; i++)
+                {
+                    Response.Write(tab + dr[i].ToString());
+                    tab = "\t";
+                }
+                Response.Write("\n");
+            }
+
+
+            Response.End();
+        }
+        #endregion
+
+
+    }
+}
